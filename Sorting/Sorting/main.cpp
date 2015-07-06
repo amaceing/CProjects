6a38a41c2a98bd171a8720e2591320ca751ec7e1
Anthony Mace <amace93@gmail.com>
2015-07-06 15:29:56 -0700

    Starting Sorting project for sorting algos for CSE310

---
+++ Sorting/Sorting/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sorting
+//
+//  Created by Anthony Mace on 7/6/15.
+//  Copyright (c) 2015 Mace. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
